d437556f0670736c7fa5f72bf10199972c70f05f
GabrielFePL <gabriellarocca0@gmail.com>
2026-07-31 21:01:45 -0300

    Projeto Artes

---
+++ src/tela_6/tela6.js
@@ -0,0 +1,30 @@
+const hotspots = document.querySelectorAll(".hotspot");
+
+function clearPressedState() {
+  hotspots.forEach((hotspot) => hotspot.classList.remove("is-pressed"));
+}
+
+hotspots.forEach((hotspot) => {
+  hotspot.addEventListener("pointerdown", () => {
+    clearPressedState();
+    hotspot.classList.add("is-pressed");
+  });
+
+  hotspot.addEventListener("pointerup", clearPressedState);
+  hotspot.addEventListener("pointercancel", clearPressedState);
+  hotspot.addEventListener("pointerleave", clearPressedState);
+});
+
+window.addEventListener("pageshow", clearPressedState);
+window.addEventListener("blur", clearPressedState);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && document.activeElement instanceof HTMLElement) {
+    document.activeElement.blur();
+  }
+});
+
+// Acrescente ?debug=1 ao endereço para visualizar as áreas clicáveis.
+if (new URLSearchParams(window.location.search).get("debug") === "1") {
+  document.documentElement.dataset.debug = "true";
+}
